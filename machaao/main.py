bd049dfa8c3a3360f268f48deaade9dd06a31767
Harshal Dhir <harshaldhir@ip-192-168-86-250.ec2.internal>
2020-08-30 10:45:38 -0700

    proposed changes with mx tunneling flow - not the absolute awesome way - but please review - @abhi

--- machaao/main.py
+++ machaao/main.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='Live And HTTPS Localhost')
     parser.add_argument('-p', '--port', type=int, default=False, help='Port number of the local server')
     parser.add_argument('-V', '--version', action='store_true' ,help='Version number of jpmx-tunnel')
-    parser.add_argument
+    # parser.add_argument
     args = parser.parse_args()
 
     if args.version:
@@ -24,9 +24,9 @@ def main():
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
-            open_tunnel(
+            await open_tunnel(
                 ws_uri=f'wss://mx.tunnel.messengerx.io/_ws/?username=&port={args.port}',
-                http_uri=f'http://127.0.0.1:{args.port}',
+                http_uri=f'http://127.0.0.1:{args.port}'
             )
         )
     except KeyboardInterrupt:
--- machaao/mxtunnel.py
+++ machaao/mxtunnel.py
@@ -11,13 +11,22 @@ ssl_context = ssl.create_default_context()
 ssl_context.load_verify_locations(certifi.where())
 
 
-async def open_tunnel(ws_uri: str, http_uri):
+async def open_tunnel(ws_uri: str, http_uri: str):
     async with websockets.connect(ws_uri, ssl=ssl_context) as websocket:
         message = json.loads(await websocket.recv())
         host, token = message["host"], message["token"]
-        print(f"Online at https://{host}/")
+        print(f"Starting secured connection with https://{host}/")
 
         client = Client(http_uri, token)
         while True:
             message = json.loads(await websocket.recv())
             asyncio.ensure_future(client.process(message, websocket))
+
+
+async def generate_host_url(ws_uri: str):
+    async with websockets.connect(ws_uri, ssl=ssl_context) as websocket:
+        message = json.loads(await websocket.recv())
+        host, token = message["host"], message["token"]
+        # print(f"Online at https://{host}/")
+        return host
+
